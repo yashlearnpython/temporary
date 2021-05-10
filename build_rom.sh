20d9d3ce40b7a312b519d829ae40d0d656e0fc31
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-10 19:21:25 +0600

    whyred-p404-ItsVixano

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # sync rom
-repo init --depth=1 -u git://github.com/crdroidandroid/android.git -b 11.0 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/boedhack/local_manifest.git --depth 1 -b 11.0 .repo/local_manifests --depth=1
+repo init -u https://github.com/P-404/platform_manifest -b rippa --depth=1 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Project404-whyred/local_manifests.git --depth=1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
-lunch lineage_mojito-userdebug
+lunch p404_whyred-userdebug
 mka bacon
 
 # upload rom
-time rclone copy out/target/product/mojito/*Unofficial-test.zip cirrus:mojito -P
+time rclone copy out/target/product/whyred/*.zip cirrus:whyred -P
