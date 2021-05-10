c6652f223be1e306649e34aa9b3e54c1e11cf3c3
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-10 19:45:55 +0600

    RMX2020-ForkLineageOS-sarthakroy2002

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 
 # sync rom
 repo init --depth=1 -u git://github.com/ForkLineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Realme-G70-Series/local_manifest.git -b 11-staging --depth=1 .repo/local_manifests
+git clone https://github.com/Realme-G70-Series/local_manifest.git -b 11-staging --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
