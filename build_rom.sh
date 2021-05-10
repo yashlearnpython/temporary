d8b966d895a3ec2b6fcb1a87ed23e3275600c435
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-10 19:24:02 +0600

    whyred-p404-ItsVixano

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 
 # sync rom
 repo init -u https://github.com/P-404/platform_manifest -b rippa --depth=1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Project404-whyred/local_manifests.git --depth=1 .repo/local_manifests
+git clone https://github.com/Project404-whyred/local_manifests.git .repo/local_manifests --depth=1
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
